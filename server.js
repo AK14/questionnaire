904e325bd343c90b32337a5759dd06d7b5bd352a
AK14 <hooploop14@gmail.com>
2022-07-25 23:03:26 +0300

    add connect to db

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ const express = require ('express');
 const app = express ();
 app.use(express.json())
 
+mongoose.connect('mongodb://hooploop14@gmail.com:Sasha68913040@77c7cbc9-5796-41a7-9e0b-00cc1fdef3ad.questionnaire-200.mongo.a.osc-fr1.scalingo-dbs.com:39447/questionnaire-200?replicaSet=questionnaire-200-rs0&ssl=true',
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
 
 // app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/js'));
@@ -20,6 +34,10 @@ app.use('/js', express.static(__dirname + '/node_modules/@poperjs/core/dist'));
 
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/quests', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 });
 
